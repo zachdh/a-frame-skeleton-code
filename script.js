4b18759766e29a5c0fd7bff7d3df1116df3fbd41
Ms. Orret <orret.deborah@pusd.us>
2023-11-14 18:27:28 +0000

    syntax errors

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ function createBoxEntity(color) {
 
     const entity = document.createElement("entity");
     let box = document.createElement("a-box")
-    box.setAttribute("position") = "-1 0.5 -3"
-    box.setAttribute("rotation") = "0 45 0"
-    box.setAttribute("color") = color
+    box.setAttribute("position", "-1 0.5 -3")
+    box.setAttribute("rotation", "0 45 0")
+    box.setAttribute("color", color)
     entity.appendChild(box)
 
     let box2 = document.createElement("a-box")
-    box2.setAttribute("position") = "-1 0.5 -3"
-    box2.setAttribute("rotation") = "0 15 0"
-    box2.setAttribute("color") = color
+    box2.setAttribute("position", "-1 0.5 -3")
+    box2.setAttribute("rotation", "0 15 0")
+    box2.setAttribute("color", color)
     entity.appendChild(box2)
     
     return entity
@@ -21,5 +21,3 @@ function createBoxEntity(color) {
 for (var i =0; i < 2; i++) {
     document.getElementById("scene").appendChild(createBoxEntity("red"))
 }
-
-
